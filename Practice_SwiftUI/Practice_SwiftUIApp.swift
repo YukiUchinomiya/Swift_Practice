f67a410ecdf00860cae84ce87d4b224478a042c4
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-07-21 23:10:54 +0900

    Initial Commit

---
+++ Practice_SwiftUI/Practice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_SwiftUIApp.swift
+//  Practice_SwiftUI
+//
+//  Created by yuki uchinomiya on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Practice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
