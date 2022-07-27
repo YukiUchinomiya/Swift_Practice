6dda9bce0ed48dac16bbd049d6464b48c2a2dd53
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-07-27 23:40:19 +0900

    fix:Generics

--- Practice_SwiftUI/Practice_SwiftUIApp.swift
+++ Practice_SwiftUI/Practice_SwiftUIApp.swift
@@ -20,3 +20,21 @@ func greet(person: String) -> String {
     let greeting = "Hello, " + person + "!"
     return greeting
 }
+
+enum Stateful<Value> {
+    case idle
+    case loading
+    case failed(Error)
+    case loaded(Value)
+}
+
+var data: Stateful<[String]> = .idle
+// データ取得中
+data = .loading
+// データ取得失敗
+data = .failed(DummyError())
+// データ取得完了
+data = .loaded(["data1", "data2", "data3"])
+
+// 他の型でも利用可能
+var anotherData: Stateful<[Int]> = .loaded([1, 2, 3])
