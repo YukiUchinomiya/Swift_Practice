912f4e4e1b69b6ef394a85cb542246547b921a86
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-07-29 22:58:58 +0900

    fix:詳細画面作成

--- Practice_SwiftUI/Views/RepoDetailView.swift
+++ Practice_SwiftUI/Views/RepoDetailView.swift
@@ -11,18 +11,26 @@ struct RepoDetailView: View {
     let repo: Repo
     
     var body: some View {
-        HStack {
-            Image("GitHubMark")
-                .resizable()
-                .frame(width: 16.0,height: 16.0)
-            VStack(alignment: .leading) {
+        VStack(alignment: .leading){
+            HStack {
+                Image("GitHubMark")
+                    .resizable()
+                    .frame(width: 16.0,height: 16.0)
                 Text(repo.owner.name)
                     .font(.caption)
+            }
                 Text(repo.name)
                     .font(.body)
                     .fontWeight(.bold)
+            Text(repo.description)
+                .padding(.top, 4)
+            HStack{
+                Image(systemName: "star")
+                Text("\(repo.stargazersCount) stars")
+                }
+            .padding(.top, 8)
             }
-        }    }
+        }
 }
 
 struct RepoDetailView_Previews: PreviewProvider {
