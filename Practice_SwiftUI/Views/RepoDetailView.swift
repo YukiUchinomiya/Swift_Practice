3b15cbc8b7f4597798c14209828581bc437020cb
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-07-30 22:00:49 +0900

    fix:スクロール処理、デザイン修正

--- Practice_SwiftUI/Views/RepoDetailView.swift
+++ Practice_SwiftUI/Views/RepoDetailView.swift
@@ -11,26 +11,34 @@ struct RepoDetailView: View {
     let repo: Repo
     
     var body: some View {
-        VStack(alignment: .leading){
-            HStack {
-                Image("GitHubMark")
-                    .resizable()
-                    .frame(width: 16.0,height: 16.0)
-                Text(repo.owner.name)
-                    .font(.caption)
-            }
-                Text(repo.name)
-                    .font(.body)
-                    .fontWeight(.bold)
-            Text(repo.description)
-                .padding(.top, 4)
+        ScrollView{
             HStack{
-                Image(systemName: "star")
-                Text("\(repo.stargazersCount) stars")
+                VStack(alignment: .leading){
+                    HStack {
+                        Image("GitHubMark")
+                            .resizable()
+                            .frame(width: 16.0,height: 16.0)
+                        Text(repo.owner.name)
+                            .font(.caption)
+                    }
+                        Text(repo.name)
+                            .font(.body)
+                            .fontWeight(.bold)
+                    Text(repo.description)
+                        .padding(.top, 4)
+                    HStack{
+                        Image(systemName: "star")
+                        Text("\(repo.stargazersCount) stars")
+                        }
+                    .padding(.top, 8)
+                    Spacer()
                 }
-            .padding(.top, 8)
+               Spacer()
             }
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
         }
+    }
 }
 
 struct RepoDetailView_Previews: PreviewProvider {
