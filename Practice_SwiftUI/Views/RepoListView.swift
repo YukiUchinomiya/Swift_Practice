d46d1883f3782588b93a49cef50bd7c5072cd8cd
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-08-06 00:27:36 +0900

    fix:モックデータ読み込み部分実装

--- Practice_SwiftUI/Views/RepoListView.swift
+++ Practice_SwiftUI/Views/RepoListView.swift
@@ -1,28 +1,35 @@
-//
-//  ContentView.swift
-//  Practice_SwiftUI
-//
-//  Created by yuki uchinomiya on 2022/07/21.
-//
-
 import SwiftUI
+import Combine
 
 struct RepoListView: View {
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
-        
+    @State private var mockRepos: [Repo] = []
+    
     var body: some View {
             NavigationView{
-                List(mockRepos) { repo in
-                    NavigationLink(
-                        destination: RepoDetailView(repo: repo)) {
-                        RepoRow(repo: repo)
+                if mockRepos.isEmpty {
+                    ProgressView("loading...")
+                }else{
+                    List(mockRepos) { repo in
+                        NavigationLink(
+                            destination: RepoDetailView(repo: repo)) {
+                            RepoRow(repo: repo)
+                        }
                     }
+                    .navigationTitle("Repositories")
                 }
-            .navigationTitle("Repositories")
         }
+            .onAppear{
+                loadRepos()
+            }
     }
+    private func loadRepos() {
+            // 1秒後にモックデータを読み込む
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                mockRepos = [
+                    .mock1, .mock2, .mock3, .mock4, .mock5
+                ]
+            }
+        }
 }
 
     struct RepoListView_Previews: PreviewProvider {
