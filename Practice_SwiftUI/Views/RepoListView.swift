eedb9e06f95334d77b39aaeb9f41c0e18f57f06e
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-08-16 00:13:01 +0900

    fix:エラーハンドリング追加

--- Practice_SwiftUI/Views/RepoListView.swift
+++ Practice_SwiftUI/Views/RepoListView.swift
@@ -4,6 +4,8 @@ import Combine
 @MainActor
 class ReposStore: ObservableObject {
     @Published private(set) var repos = [Repo]()
+    @Published private(set) var error: Error? = nil
+    @Published private(set) var isLoading: Bool = false
 
     func loadRepos() async {
         let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
@@ -13,13 +15,24 @@ class ReposStore: ObservableObject {
         urlRequest.allHTTPHeaderFields = [
             "Accept": "application/vnd.github.v3+json"
         ]
+        
+        isLoading = true
 
-        let (data, _) = try! await URLSession.shared.data(for: urlRequest)
+        do {
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            
+        //guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        // }
         
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        let value = try! decoder.decode([Repo].self, from: data)
+        let value = try decoder.decode([Repo].self, from: data)
         repos = value
+        } catch {
+            self.error = error
+            isLoading = false
+        }
     }
 }
 
@@ -28,16 +41,39 @@ struct RepoListView: View {
     
     var body: some View {
             NavigationView{
-                if reposStore.repos.isEmpty {
-                    ProgressView("loading...")
-                }else{
-                    List(reposStore.repos) { repo in
-                        NavigationLink(
-                            destination: RepoDetailView(repo: repo)) {
-                            RepoRow(repo: repo)
+                if reposStore.error != nil {
+                        VStack{
+                            Group {
+                                Image("GitHubMark")
+                                Text("Failed to load repositories")
+                                    .padding(.top, 4)
+                            }
+                            .foregroundColor(.black)
+                            .opacity(0.4)
+                            Button(
+                                action: {
+                                    Task {
+                                        await reposStore.loadRepos() // リトライボタンをタップしたときに再度リクエストを投げる
+                                    }
+                                },
+                                label: {
+                                    Text("Retry")
+                                        .fontWeight(.bold)
+                                }
+                            )
+                            .padding(.top, 8)
                         }
-                    }
-                    .navigationTitle("Repositories")
+                    }else{
+                        if reposStore.isLoading {
+                            ProgressView("loading...")
+                        } else {
+                        List(reposStore.repos) { repo in
+                            NavigationLink(
+                                destination: RepoDetailView(repo: repo)) {
+                                RepoRow(repo: repo)
+                            }
+                        }
+                        .navigationTitle("Repositories")
                 }
         }
             .task{
